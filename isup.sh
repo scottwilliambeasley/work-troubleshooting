acebde6d161cce018e4174ebc980a0eb8b646e3d
Scott Beasley <19320511+scottwilliambeasley@users.noreply.github.com>
2018-04-17 08:05:31 -0500

    add common ports for MySQL and MSSQL

--- isup.sh
+++ isup.sh
@@ -10,8 +10,7 @@ getPingResult() {
                 canPing="unresponsive"
         fi
 
-        echo $canPing; 
-
+        echo $canPing;
 }
 
 getInterfaceType () {
@@ -31,39 +30,36 @@ getInterfaceType () {
 showConnectionResults () {
 
         echo "";
+
         setterm -term linux -back red;
                 echo -n "$interfaceType Interface - $interface";
         setterm -default;
+
         echo "";
-
-
 
         echo "ICMP  : $canPing";
         echo "SSH   : $sshStatus";
         echo "HTTP  : $httpStatus";
         echo "HTTPS : $httpsStatus";
         echo "RDP   : $rdpStatus";
-        echo "";
-
+        echo "MYSQL : $mysqlStatus";
+        echo "MSSQL : $mssqlStatus";
 }
-
 
 for interface in "$@"; do
 
         canPing=$(getPingResult $interface)
-
         interfaceType=$(getInterfaceType $interface)
 
-        nmapResult=$(nmap -Pn -p 22,80,443,3389 $interface);
-
-                sshStatus=$(echo   "$nmapResult" | grep '22/tcp'   | awk '{print $2}' );
-
-                httpStatus=$(echo  "$nmapResult" | grep '80/tcp'   | awk '{print $2}' );
-
-                httpsStatus=$(echo "$nmapResult" | grep '443/tcp'  | awk '{print $2}' );
-
-                rdpStatus=$(echo   "$nmapResult" | grep '3389/tcp' | awk '{print $2}' );
+        nmapResult=$(nmap -Pn -p 22,80,443,3306,1433,3389 $interface);
+                sshStatus=$(echo    "$nmapResult" | grep '22/tcp'   | awk '{print $2}' );
+                httpStatus=$(echo   "$nmapResult" | grep '80/tcp'   | awk '{print $2}' );
+                httpsStatus=$(echo  "$nmapResult" | grep '443/tcp'  | awk '{print $2}' );
+                rdpStatus=$(echo    "$nmapResult" | grep '3389/tcp' | awk '{print $2}' );
+                mysqlStatus=$(echo  "$nmapResult" | grep '3306/tcp' | awk '{print $2}' );
+                mssqlStatus=$(echo  "$nmapResult" | grep '1433/tcp' | awk '{print $2}' );
 
         showConnectionResults;
 
 done
+
